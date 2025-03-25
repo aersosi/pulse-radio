e2e3d2ea7cd4b513aae4cdae8b0c1f7e42b408ef
aersosi <acerelroy@gmail.com>
2025-03-25 15:05:11 +0100

    fix: vercel build

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,8 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  ignores: ["backend/"]
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
