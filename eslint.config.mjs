06aacb51304e90bf3fcf96406ceb0543b72aee06
aersosi <acerelroy@gmail.com>
2025-03-25 12:13:05 +0100

    fix: vercel build fails -> eslint should ignore ./backend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,11 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  ignores: ["backend/"]
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
